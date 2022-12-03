d0126e660bfa17b56ed04d8cbf934562a35c80a2
IGardocki <ian.d.gardocki@gmail.com>
2022-12-02 22:02:53 -0600

    combined into one card

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,31 @@ import holidayBackground from './holidayBackground.png'
 function App() {
 
   return (
-    <Container fluid style={{ display: "flex", flexDirection: "column", minHeight: "100vh", textAlign: "center", alignItems:"center", justifyContent: "space-around", backgroundImage: `url(${holidayBackground})`, backgroundSize: 'cover' }}>
+    <Container fluid style={{ display: "flex", flexDirection: "column", minHeight: "100vh", maxWidth: "100vw", textAlign: "center", alignItems: "center", justifyContent: "space-around", backgroundImage: `url(${holidayBackground})`, backgroundSize: 'cover' }}>
 
-      <Card><h1>Hey friend! Sounds like you've gotten the JackBox Party Pack 9 as a Secret Santa gift!</h1>
-      </Card>
 
-      <Card>
+      <Card style={{ minHeight: "80vh" }}>
+
+        <Card.Title><h1>Hey friend! Sounds like you've gotten the JackBox Party Pack 9 as a Secret Santa gift!</h1></Card.Title>
+        <br />
         <Card.Title>Oh, neat. How do I get it?</Card.Title>
-        <Card.Body>Well! You can download your digital products from:<br/>
+        <Card.Body>Well! You can download your digital products from: {` `}
           <a href='https://transactions.sendowl.com/orders/117121526/download/fa132ec9d3a894a9191b56a41c54c9b7'>this link</a>
         </Card.Body>
+
         <Card.Body>
           In order to use some of the products in this order you will require the following Steam code:
-          <br/>QXD5P-0DDGF-LZWE6
+          <br />
+          QXD5P-0DDGF-LZWE6
         </Card.Body>
-        </Card>
-        <Card>
-          <Card.Title>In case you have problems, here's a video to guide you through the process!</Card.Title>
-        </Card>
- 
+
+        <Card.Title>In case you have problems, here's a video to guide you through the process!</Card.Title>
+        <Card.Body>
           <iframe width="320" height="180" src="https://www.youtube.com/embed/TvJcC79mT34" title="How to Redeem Steam Codes for Jackbox Games" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-     
+        </Card.Body>
+
+      </Card>
     </Container>
-
-
-
-
-
   );
 }
 
